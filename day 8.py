4949db347413ac6d8e5b892b7c99214a8d5baaef
selvamdivya <selvamdivya177@gmail.com>
2026-07-27 20:20:19 +0530

    "day 8 problem"

---
+++ day 8.py
@@ -0,0 +1,27 @@
+class Solution:
+    def myAtoi(self, s):
+        i = 0
+        n = len(s)
+
+        while i < n and s[i] == ' ':
+            i += 1
+
+        sign = 1
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            if s[i] == '-':
+                sign = -1
+            i += 1
+
+        num = 0
+        while i < n and s[i].isdigit():
+            num = num * 10 + int(s[i])
+            i += 1
+
+        num *= sign
+
+        if num < -2147483648:
+            return -2147483648
+        if num > 2147483647:
+            return 2147483647
+
+        return num
