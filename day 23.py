a060f04eac295444bef99955ddcb0e764e444ad8
selvamdivya <selvamdivya177@gmail.com>
2026-08-10 20:38:13 +0530

    "day 23 problem"

---
+++ day 23.py
@@ -0,0 +1,32 @@
+class Solution:
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+
+        def mergeTwoLists(l1, l2):
+            dummy = ListNode(0)
+            current = dummy
+
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    current.next = l1
+                    l1 = l1.next
+                else:
+                    current.next = l2
+                    l2 = l2.next
+
+                current = current.next
+
+            if l1:
+                current.next = l1
+            else:
+                current.next = l2
+
+            return dummy.next
+
+        result = None
+
+        for lst in lists:
+            result = mergeTwoLists(result, lst)
+
+        return result
