68229481aa5b9c2cee682c1de688c9a570b419f5
selvamdivya <selvamdivya177@gmail.com>
2026-08-11 19:03:54 +0530

    "day 24 problem"

---
+++ day 24.py
@@ -0,0 +1,24 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def swapPairs(self, head):
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        while prev.next and prev.next.next:
+            first = prev.next
+            second = first.next
+
+            # Swap
+            prev.next = second
+            first.next = second.next
+            second.next = first
+
+            # Move to next pair
+            prev = first
+
+        return dummy.next
